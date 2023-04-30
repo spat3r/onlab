117d06b2301d644acb8226e000732954800844b7
speti <halasi.p2000@gmail.com>
2023-04-30 18:38:58 +0200

    chaned codingstyle, upgraded to systemverilog

--- functional_rtl/dsp_25x18.sv
+++ functional_rtl/dsp_25x18.sv
@@ -23,33 +23,34 @@ module dsp_25x18
    parameter A_REG = 2,
    parameter B_REG = 2
 )(
-   input                clk,
-   input  signed [24:0] a,
-   input  signed [17:0] b,
-   input  signed [47:0] pci, 
-   output signed [47:0] p
+   input  logic               clk,
+   input  logic signed [24:0] a,
+   input  logic signed [17:0] b,
+   input  logic signed [47:0] pci, 
+   output logic signed [47:0] p
 );
 
-reg signed [24:0] a_reg[A_REG-1:0];
-reg signed [17:0] b_reg[B_REG-1:0];
-reg signed [42:0] m_reg;
-reg signed [47:0] p_reg;
-integer i;
+    logic signed [24:0] a_reg[A_REG-1:0];
+    logic signed [17:0] b_reg[B_REG-1:0];
+    logic signed [42:0] m_reg;
+    logic signed [47:0] p_reg;
+    integer i;
 
-always @ (posedge clk)
-for (i=0; i<A_REG; i=i+1)
-   a_reg[i] <= (i==0) ? a : a_reg[i-1];
+    always @ (posedge clk) begin
+        for (i=0; i<A_REG; i=i+1)
+            a_reg[i] <= (i==0) ? a : a_reg[i-1];
+    end
 
-always @ (posedge clk)
-for (i=0; i<B_REG; i=i+1)
-   b_reg[i] <= (i==0) ? b : b_reg[i-1];
+    always @ (posedge clk) begin
+        for (i=0; i<B_REG; i=i+1)
+            b_reg[i] <= (i==0) ? b : b_reg[i-1];
+    end
 
-always @ (posedge clk)
-begin
-   m_reg <= a_reg[A_REG-1]*b_reg[B_REG-1];
-   p_reg <= m_reg + pci;
-end
+    always @ (posedge clk) begin
+        m_reg <= a_reg[A_REG-1]*b_reg[B_REG-1];
+        p_reg <= m_reg + pci;
+    end
 
-assign p = p_reg;
+    assign p = p_reg;
 
 endmodule
