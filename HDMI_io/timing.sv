2826477bac4865c02431939e4d287fceb4e2ce45
speti <halasi.p2000@gmail.com>
2023-05-01 16:33:33 +0200

    change coding style to lowRISC sv reworked h_blank to be easier to use in testbench

--- HDMI_io/timing.sv
+++ HDMI_io/timing.sv
@@ -11,17 +11,17 @@ module vga_timing #(
         parameter V_POLARITY       = 1'b1
     )(
         //Órajel és reset.
-        input wire clk, //Pixel órajel bemenet.
-        input wire rst, //Reset bemenet.
+        input logic clk, //Pixel órajel bemenet.
+        input logic rst, //Reset bemenet.
 
         //Az aktuális pixel pozíció.
-        output reg [10:0] h_cnt = 11'd0, //X-koordináta.
-        output reg [9:0] v_cnt = 10'd0, //Y-koordináta.
+        output logic [10:0] h_cnt = 11'd0, //X-koordináta.
+        output logic [9:0] v_cnt = 10'd0, //Y-koordináta.
 
         //Szinkron és kioltó jelek.
-        output reg h_sync = 1'b1, //Horizontális szinkron pulzus.
-        output reg v_sync = 1'b0, //Vertikális szinkron pulzus.
-        output wire blank //Kioltó jel.
+        output logic h_sync = 1'b1, //Horizontális szinkron pulzus.
+        output logic v_sync = 1'b0, //Vertikális szinkron pulzus.
+        output logic blank //Kioltó jel.
     );
 
 //******************************************************************************
@@ -43,76 +43,58 @@ localparam V_BLANK_END      = V_SYNC_END + V_BACK_PORCH;
 //* A horizontális és vertikális számlálók. *
 //******************************************************************************
 
-always @(posedge clk)
-begin
-    if (rst || (h_cnt == H_BLANK_END))
-        h_cnt <= 12'd0;
-    else
-        h_cnt <= h_cnt + 12'd1;
+always_ff @(posedge clk) begin
+    if (rst || (h_cnt == H_BLANK_END)) h_cnt <= 12'd0;
+    else                               h_cnt <= h_cnt + 12'd1;
 end
-always @(posedge clk)
-begin
-    if (rst)
-        v_cnt <= 11'd0;
-    else
-    if (h_cnt == H_BLANK_END)
-        if (v_cnt == V_BLANK_END)
-            v_cnt <= 11'd0;
-        else
-            v_cnt <= v_cnt + 11'd1;
+
+always_ff @(posedge clk) begin
+    if (rst) v_cnt <= 11'd0;
+    else if (h_cnt == H_BLANK_END) begin
+        if (v_cnt == V_BLANK_END) v_cnt <= 11'd0;
+        else                      v_cnt <= v_cnt + 11'd1;
+    end
 end
 
 //******************************************************************************
 //* A szinkron pulzusok generálása. *
 //******************************************************************************
 
-always @(posedge clk)
-begin
-    if (rst)
-        h_sync <= ~H_POLARITY;
-    else if (h_cnt == H_SYNC_BEGIN)
-        h_sync <= H_POLARITY;
-    else if (h_cnt == H_SYNC_END)
-        h_sync <= ~H_POLARITY;
+always_ff @(posedge clk) begin
+    if (rst)                        h_sync <= ~H_POLARITY;
+    else if (h_cnt == H_SYNC_BEGIN) h_sync <= H_POLARITY;
+    else if (h_cnt == H_SYNC_END)   h_sync <= ~H_POLARITY;
 end
 
 always @(posedge clk)
 begin
-    if (rst)
-        v_sync <= ~V_POLARITY;
-    else
-        if (h_cnt == H_BLANK_END)
-            if (v_cnt == V_SYNC_BEGIN)
-                v_sync <= V_POLARITY;
-    else
-        if (v_cnt == V_SYNC_END)
-            v_sync <= ~V_POLARITY;
+    if (rst) v_sync <= ~V_POLARITY;
+    else if (h_cnt == H_BLANK_END) begin
+            if (v_cnt == V_SYNC_BEGIN)    v_sync <=  V_POLARITY;
+            else if (v_cnt == V_SYNC_END) v_sync <= ~V_POLARITY;
+    end
 end
 
 //******************************************************************************
 //* A kioltó jel előállítása. *
 //******************************************************************************
 
-reg h_blank = 1'b0;
-reg v_blank = 1'b0;
+logic h_blank = 1'b1;
+logic v_blank = 1'b1;
 
-always @(posedge clk)
-begin
-    if (rst || (h_cnt == H_BLANK_END))
-        h_blank <= 1'b0;
-    else if (h_cnt == H_BLANK_BEGIN)
-        h_blank <= 1'b1;
+always_ff @(posedge clk) begin
+    if (rst)                         h_blank <= 1'b1;
+    else if (h_cnt <= H_BLANK_BEGIN) h_blank <= 1'b0;
+    else if (h_cnt <= H_BLANK_END)   h_blank <= 1'b1;
+    else                             h_blank <= 1'b0;
 end
 
-always @(posedge clk)
-begin
-    if (rst)
-        v_blank <= 1'b0;
-    else if (h_cnt == H_BLANK_END)
-        if (v_cnt == V_BLANK_BEGIN)
-            v_blank <= 1'b1;
-        else if (v_cnt == V_BLANK_END)
-            v_blank <= 1'b0;
+always_ff @(posedge clk) begin
+    if (rst) v_blank <= 1'b0;
+    else if (h_cnt == H_BLANK_END) begin
+        if (v_cnt == V_BLANK_BEGIN)    v_blank <= 1'b1;
+        else if (v_cnt == V_BLANK_END) v_blank <= 1'b0;
+    end
 end
 assign blank = h_blank | v_blank;
 endmodule
