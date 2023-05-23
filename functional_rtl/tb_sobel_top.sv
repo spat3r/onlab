dd7dde7269f3a77e9a9e167309e81fed13619ed0
speti <halasi.p2000@gmail.com>
2023-05-23 22:42:35 +0200

    adding tester module to tb env

--- functional_rtl/tb_sobel_top.sv
+++ functional_rtl/tb_sobel_top.sv
@@ -4,66 +4,122 @@ module tb_sobel_top ();
 
 parameter COLORDEPTH = 8;
 parameter POWEREFF = 2;
+parameter SCREENHEIGHT = 64;
+parameter SCREENWIDTH = 64;
+parameter POL_VS = 1;
+parameter POL_HS = 1;
 
-logic        clk =1;
-logic        rst =1;
-logic [23:0] rgb_i;
-logic [23:0] rgb_o;
-logic        dv_i;
-logic        hs_i;
-logic        vs_i, vs_o;
+logic rx_clk = 0;    // pixelclock, @1600x900 60Hz : XXX MHz
+parameter RX_CLK_PERIOD_DELAY   = 10.025;
 
-always #5 clk <= ~clk;
+logic [7:0] led_r;
+logic [7:0] switch;
+logic [3:0] bt;
+logic rstbt;     // rstbutton, FPGA dedicated RST button
+
+logic        DUV_dv_i, DUV_dv_o, VGA_dv;
+logic        DUV_vs_i, DUV_vs_o, VGA_vs;
+logic        DUV_hs_i, DUV_hs_o, VGA_hs;
+logic        blank;
+logic [11:0] v_cnt, h_cnt;
+logic  [7:0] DUV_red_i,    DUV_red_o;
+logic  [7:0] DUV_green_i,  DUV_green_o;
+logic  [7:0] DUV_blue_i,   DUV_blue_o;
+
+always #5 rx_clk <= ~rx_clk;
 
 initial begin
-   rst <= 1;
-   #102 
-   rst <= 0;
+    rstbt <= 0;
+    #15
+    rstbt <= 1;
+    #214 
+    rstbt <= 0;
+    switch <= 5'd4;
 
-   @(posedge vs_o)
-   @(negedge vs_o)
-   @(posedge hs_o)
-   $stop;
+    @(posedge DUV_vs_o)
+    @(negedge DUV_vs_o)
+    @(posedge DUV_hs_o)
+    $stop;
 end
 
+regressionTester #(
+    .VRES(SCREENHEIGHT),
+    .HRES(SCREENWIDTH)
+) rgtr_inst (
+    .clk        ( rx_clk          ),
+    .rst        ( rstbt           ),
+    .h_cnt      ( h_cnt           ),
+    .v_cnt      ( v_cnt           ),
+    .vga_dv_o   ( VGA_dv          ),
+    .vga_hs_o   ( VGA_hs          ),
+    .vga_vs_o   ( VGA_vs          ),
 
+    .tb_red_o   ( DUV_red_o       ),
+    .tb_green_o ( DUV_green_o     ),
+    .tb_blue_o  ( DUV_blue_o      ),
+    .tb_dv_o    ( DUV_dv_o        ),
+    .tb_hs_o    ( DUV_hs_o        ),
+    .tb_vs_o    ( DUV_vs_o        ),
+
+    .tb_red_i   ( DUV_red_i       ),
+    .tb_green_i ( DUV_green_i     ),
+    .tb_blue_i  ( DUV_blue_i      ),
+    .tb_dv_i    ( DUV_dv_i        ),
+    .tb_hs_i    ( DUV_hs_i        ),
+    .tb_vs_i    ( DUV_vs_i        )
+);
 
 sobel_top #(
+        .COLORDEPTH(COLORDEPTH),
+        .SCREENWIDTH(SCREENWIDTH),
+        .POL_VS(POL_VS),
+        .POL_HS(POL_HS)
     ) sobel_top_inst (
-        .clk(clk),
-        .rst(rst),
-        .red_i(rgb_i [23:16]),
-        .green_i(rgb_i [15:8]),
-        .blue_i(rgb_i [7:0]),
-        .red_o(rgb_o [23:16]),
-        .green_o(rgb_o [15:8]),
-        .blue_o(rgb_o [7:0]),
-        .dv_i(dv_i),
-        .hs_i(hs_i),
-        .vs_i(vs_i),
-        .dv_o(dv_o),
-        .hs_o(hs_o),
-        .vs_o(vs_o)
+        .clk      ( rx_clk        ),
+        .rst      ( rstbt         ),
+        .sw       ( switch        ),
+        .red_i    ( DUV_red_i     ),
+        .green_i  ( DUV_green_i   ),
+        .blue_i   ( DUV_blue_i    ),
+        .red_o    ( DUV_red_o     ),
+        .green_o  ( DUV_green_o   ),
+        .blue_o   ( DUV_blue_o    ),
+        .dv_i     ( DUV_dv_i      ),
+        .hs_i     ( DUV_hs_i      ),
+        .vs_i     ( DUV_vs_i      ),
+        .dv_o     ( DUV_dv_o      ),
+        .hs_o     ( DUV_hs_o      ),
+        .vs_o     ( DUV_vs_o      )
     );
 
 vga_timing #(
+        .H_VISIBLE     ( SCREENWIDTH ),
+        .V_VISIBLE     ( SCREENHEIGHT ),
+        .H_FRONT_PORCH ( 3  ),
+        .H_SYNC_PULSE  ( 13 ),
+        .H_BACK_PORCH  ( 3  )
     )timing (
-        .clk(clk),
-        .rst(rst),
-        .h_sync(hs_i),
-        .v_sync(vs_i),
-        .blank(blank)
+        .clk        ( rx_clk ),
+        .rst        ( rstbt  ),
+        .h_sync     ( VGA_hs ),
+        .v_sync     ( VGA_vs ),
+        .h_cnt      ( h_cnt  ),
+        .v_cnt      ( v_cnt  ),
+        .blank      ( blank  )
 );
-assign dv_i = ~blank;
+assign VGA_dv = ~blank;
 
 
-logic [23:0] rgb;
-// rgb data gen
-always_ff @(posedge clk) begin
-    if (rst) rgb <= 23'had98b7;
-    else rgb <= { rgb[22:0], rgb[3] ^ rgb[8] ^ rgb[13] ^ rgb[22]}; 
-end
 
-assign rgb_i = rgb;
+
+// logic [23:0] rgb;
+// // rgb data gen
+// always_ff @(posedge rx_clk) begin
+//     rgb <= ( v_cnt[2]  ) ? 24'hFFFFFF : 24'h0; // ^ v_cnt[0]
+//     // if (rst) rgb <= 23'had98b7;
+//     // else rgb <= { rgb[22:0], rgb[3] ^ rgb[8] ^ rgb[13] ^ rgb[22]}; 
+// end
+
+// assign {red_i, green_i, blue_i} = dv_i ? rgb : 24'b0;
 
 endmodule
